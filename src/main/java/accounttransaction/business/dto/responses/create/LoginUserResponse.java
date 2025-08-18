e95252f03fe433d384fd763b92088713efc99ce8
RobertoShee <robert.shee94@gmail.com>
2025-08-17 20:46:49 -0400

    Refactor LoginUserResponse to return only JWT

--- src/main/java/accounttransaction/business/dto/responses/create/LoginUserResponse.java
+++ src/main/java/accounttransaction/business/dto/responses/create/LoginUserResponse.java
@@ -6,31 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
-import java.util.UUID;
-
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginUserResponse {
-    @JsonProperty("id")
-    private UUID id;
-
-    @JsonProperty("created")
-    private Date createdAt;
-
-    @JsonProperty("modified")
-    private Date modifiedAt;
-
-    @JsonProperty("last_login")
-    private Date lastLogin;
-
-    @JsonProperty("token")
-    private UUID token;
-
-    @JsonProperty("isactive")
-    private boolean isActive;
+    
 
     @JsonProperty("jwt")
     private String jwt;
